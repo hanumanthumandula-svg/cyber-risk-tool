f946bc3ce13bee0a07afdb3452c96bd76ef8d142
hanumanth <hanumanthumandula@gmail.com>
2026-06-09 13:56:31 +0530

    Remove unused axios import

--- src/pages/AutoAssessment.js
+++ src/pages/AutoAssessment.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const AutoAssessment = () => {
   const navigate = useNavigate();
